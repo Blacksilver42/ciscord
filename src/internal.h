2db0ee97f002b92a76005c76c3b54b77d381f68f
Connor [REDACTED] <blacksilverck35@gmail.com>
2018-01-17 18:00:26 +0000

    consistent function names

--- src/internal.h
+++ src/internal.h
@@ -1,9 +1,9 @@
 #ifndef __CDIS_INTERNAL_H__
 #define __CDIS_INTERNAL_H__
 
-extern void disC_log_info(const char * format, ...);
-extern void disC_log_warn(const char * format, ...);
-extern void disC_log_darn(const char * format, ...);
+extern void ld_log_info(const char * format, ...);
+extern void ld_log_warn(const char * format, ...);
+extern void ld_log_darn(const char * format, ...);
 
 
 
