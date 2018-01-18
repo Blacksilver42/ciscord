31c64f9f3f31a53ca5e35e1677d845703e03be6a
Connor [REDACTED] <blacksilverck35@gmail.com>
2018-01-18 17:35:59 +0000

    ld_log_file properly

--- src/log.c
+++ src/log.c
@@ -1,6 +1,8 @@
 #include <ciscord/internal.h>
+#include <stdio.h>
+#include <stdarg.h>
 
-FILE * ld_log_file = stderr;
+FILE * ld_log_file = NULL;
 
 extern void ld_log_stream(FILE * stream){
 	ld_log_file = stream;
@@ -8,26 +10,29 @@ extern void ld_log_stream(FILE * stream){
 
 
 extern void ld_log_info(const char * format, ...){
+	if(ld_log_file == NULL) ld_log_file = stdout;
 	va_list ap;
-	va_start(format, ap);
+	va_start(ap, format);
 	fprintf(ld_log_file, "[LD/INFO] ");
-	vfprintf(ld_log_file, ap);
+	vfprintf(ld_log_file, format, ap);
 	va_end(ap);
 }
 
 extern void ld_log_warn(const char * format, ...){
+	if(ld_log_file == NULL) ld_log_file = stdout;
 	va_list ap;
-	va_start(format, ap);
+	va_start(ap, format);
 	fprintf(ld_log_file, "[LD/WARN] ");
-	vfprintf(ld_log_file, ap);
+	vfprintf(ld_log_file, format, ap);
 	va_end(ap);
 }
 
 extern void ld_log_fuck(const char * format, ...){
+	if(ld_log_file == NULL) ld_log_file = stdout;
 	va_list ap;
-	va_start(format, ap);
+	va_start(ap, format);
 	fprintf(ld_log_file, "[LD/FUCK] ");
-	vfprintf(ld_log_file, ap);
+	vfprintf(ld_log_file, format, ap);
 	va_end(ap);
 }
 
